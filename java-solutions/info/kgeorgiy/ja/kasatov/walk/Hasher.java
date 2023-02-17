5ca5860ef409a08a68f0e344dc5a71699cc7aeae
Roman Kasatov <R.Kasatov@skoltech.ru>
2023-02-17 13:49:17 +0300

    add hasher

---
+++ java-solutions/info/kgeorgiy/ja/kasatov/walk/Hasher.java
@@ -0,0 +1,54 @@
+package info.kgeorgiy.ja.kasatov.walk;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+public class Hasher {
+    final static String nullHash = "0".repeat(40);
+
+    public static String calculateHash(Path path) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            try (InputStream reader = Files.newInputStream(path)) {
+                try {
+                    int read;
+                    byte[] buffer = new byte[1 << 16];
+                    while ((read = reader.read(buffer)) >= 0) {
+                        md.update(buffer, 0, read);
+                    }
+
+                    // JDK17
+                    return HexFormat.of().formatHex(md.digest());
+
+                } catch (IOException e) {
+                    return nullHash;
+                }
+            } catch (IOException e) {
+                return nullHash;
+            }
+
+        } catch (NoSuchAlgorithmException e) {
+            // Every implementation of the Java platform is required to support
+            // the following standard MessageDigest algorithms... (SHA-256 among them)
+            return nullHash;
+        }
+    }
+
+    public static Map<String, String> hashRecursively(Path path) {
+        try (Stream<Path> pathStream = Files.walk(path)) {
+            return pathStream
+                    .filter(f -> !Files.isDirectory(f))
+                    .collect(Collectors.toMap(Path::toString, Hasher::calculateHash));
+        } catch (IOException e) {
+            // File on 'path' is unreachable
+            return new HashMap<>();
+        }
+    }
+}
--- java-solutions/info/kgeorgiy/ja/kasatov/walk/RecursiveWalk.java
+++ java-solutions/info/kgeorgiy/ja/kasatov/walk/RecursiveWalk.java
@@ -6,7 +6,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Map;
-import info.kgeorgiy.ja.kasatov.walk.Hasher;
 
 
 public class RecursiveWalk {
