f7b7cb229b590c71e87e5959087695074d2eac3e
Roman Kasatov <R.Kasatov@skoltech.ru>
2023-03-03 13:39:32 +0300

    ArraySet

--- java-solutions/info/kgeorgiy/ja/kasatov/arrayset/ArraySet.java
+++ java-solutions/info/kgeorgiy/ja/kasatov/arrayset/ArraySet.java
@@ -1,4 +1,238 @@
-package info.kgeorgiy.ja.__last_name__.arrayset;
+package info.kgeorgiy.ja.kasatov.arrayset;
+import java.util.*;
 
-public class ArraySet {
+
+public class ArraySet<T> extends AbstractSet<T> implements NavigableSet<T> {
+
+    private final ArrayList<T> arrayList;
+    private final Comparator<? super T> comparator;
+    private final boolean naturalOrder;
+
+
+    public ArraySet() {
+        this(null, null, true);
+    }
+
+    public ArraySet(Collection<? extends T> collection) {
+        this(collection, null, false);
+    }
+
+    public ArraySet(Collection<? extends T> collection, Comparator<? super T> comparator) {
+        this(collection, comparator, false);
+    }
+
+    private Comparator<T> makeComparator() {
+        return (o1, o2) -> ((Comparable<T>) o1).compareTo(o2);
+    }
+
+    private ArraySet(Collection<? extends T> collection, Comparator<? super T> comparator, boolean sorted) {
+        if (comparator == null) {
+            naturalOrder = true;
+            this.comparator = makeComparator();
+        } else {
+            naturalOrder = false;
+            this.comparator = comparator;
+        }
+
+        if (collection == null) {
+            collection = Collections.emptyList();
+        } else if (!sorted) {
+            TreeSet<T> treeSet = new TreeSet<>(comparator);
+            treeSet.addAll(collection);
+            collection = treeSet;
+        }
+        arrayList = new ArrayList<>(collection);
+    }
+
+    public int getIndex(T t, boolean least, boolean inclusive) {
+        int index = Collections.binarySearch(arrayList, t, comparator);
+        if (index < 0) {
+            index = -(index + 1);
+            if (!least) {
+                index--;
+            }
+        } else {
+            if (!inclusive && comparator.compare(arrayList.get(index), t) == 0) {
+                if (least) {
+                    index++;
+                } else {
+                    index--;
+                }
+            }
+        }
+        return index;
+    }
+
+    @Override
+    public T lower(T t) {
+        int index = getIndex(t, false, false);
+        return (index < 0) ? null : arrayList.get(index);
+    }
+
+    @Override
+    public T floor(T t) {
+        int index = getIndex(t, false, true);
+        return (index < 0) ? null : arrayList.get(index);
+    }
+
+    @Override
+    public T ceiling(T t) {
+        int index = getIndex(t, true, true);
+        return (index >= arrayList.size()) ? null : arrayList.get(index);
+    }
+
+    @Override
+    public T higher(T t) {
+        int index = getIndex(t, true, false);
+        return (index >= arrayList.size()) ? null : arrayList.get(index);
+    }
+
+    @Override
+    public T pollFirst() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public T pollLast() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        return arrayList.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return arrayList.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        T element = (T)  Objects.requireNonNull(o);
+        int index = getIndex(element, false, true);
+        return index >= 0 && comparator.compare(arrayList.get(index), element) == 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return arrayList.iterator();
+    }
+
+    @Override
+    public Object[] toArray() {
+        return arrayList.toArray();
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return arrayList.toArray(a);
+    }
+
+    @Override
+    public boolean add(T t) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return c.stream().allMatch(this::contains);
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public NavigableSet<T> descendingSet() {
+        return new ArraySet<T>(arrayList, comparator.reversed());
+    }
+
+    @Override
+    public Iterator<T> descendingIterator() {
+        ArrayList<T> list = new ArrayList<>(arrayList);
+        Collections.reverse(list);
+        return list.iterator();
+    }
+
+    @Override
+    public NavigableSet<T> subSet(T fromElement, boolean fromInclusive, T toElement, boolean toInclusive) {
+        if (comparator.compare(fromElement, toElement) > 0) throw new IllegalArgumentException();
+        return subSetByRange(
+            getIndex(fromElement, true, fromInclusive),
+            getIndex(toElement, false, toInclusive) + 1
+        );
+    }
+
+    @Override
+    public NavigableSet<T> headSet(T toElement, boolean inclusive) {
+        int index = getIndex(toElement, false, inclusive);
+        return subSetByRange(0, index + 1);
+    }
+
+    @Override
+    public NavigableSet<T> tailSet(T fromElement, boolean inclusive) {
+        int index = getIndex(fromElement, true, inclusive);
+        return subSetByRange(index, arrayList.size());
+    }
+
+    @Override
+    public Comparator<? super T> comparator() {
+        return (naturalOrder) ? null : comparator;
+    }
+
+    private ArraySet<T> subSetByRange(int start, int end) {
+        if (end < start) {
+            return new ArraySet<T>(null, comparator(), true);
+        }
+        return new ArraySet<>(arrayList.subList(start, end), comparator(), true);
+    }
+
+    @Override
+    public SortedSet<T> subSet(T fromElement, T toElement) {
+        return subSet(fromElement, true, toElement, false);
+    }
+
+    @Override
+    public SortedSet<T> headSet(T toElement) {
+        return headSet(toElement, false);
+    }
+
+    @Override
+    public SortedSet<T> tailSet(T fromElement) {
+        return tailSet(fromElement, true);
+    }
+
+    @Override
+    public T first() {
+        if (arrayList.isEmpty()) throw new NoSuchElementException();
+        return arrayList.get(0);
+    }
+
+    @Override
+    public T last() {
+        if (arrayList.isEmpty()) throw new NoSuchElementException();
+        return arrayList.get(arrayList.size() - 1);
+    }
 }
