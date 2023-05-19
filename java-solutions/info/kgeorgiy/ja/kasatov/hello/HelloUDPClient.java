461820f84bf3b1e77a8ae999b9bde1e7e55f029f
Roman Kasatov <R.Kasatov@skoltech.ru>
2023-05-19 13:33:26 +0300

    hello

---
+++ java-solutions/info/kgeorgiy/ja/kasatov/hello/HelloUDPClient.java
@@ -0,0 +1,107 @@
+package info.kgeorgiy.ja.kasatov.hello;
+
+import info.kgeorgiy.java.advanced.hello.HelloClient;
+
+import java.io.IOException;
+import java.net.*;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.*;
+
+public class HelloUDPClient implements HelloClient {
+
+    @Override
+    public void run(String host, int port, String prefix, int threads, int requests) {
+        SocketAddress serverAddres;
+        try {
+            serverAddres = new InetSocketAddress(InetAddress.getByName(host), port);
+        } catch (UnknownHostException e) {
+            System.err.println("Can't resolve host: " + e.getMessage());
+            return;
+        }
+
+        ExecutorService threadsPool = Executors.newFixedThreadPool(threads);
+
+        for (int threadNmb = 0; threadNmb < threads; threadNmb++) {
+            threadsPool.submit(new ClientThread(threadNmb, requests, serverAddres, prefix));
+        }
+
+        try {
+            threadsPool.shutdown();
+            threadsPool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            System.err.println("Client threads was interrupted");
+        }
+    }
+
+    private class ClientThread implements Runnable {
+
+        private final int threadNmb;
+        private final int requests;
+        private final String prefix;
+        private int requestNmb = 0;
+        private final SocketAddress serverAddress;
+        private int BUFF_SIZE;
+
+        private String createRequestText() {
+            return prefix + (threadNmb + 1) + "_" + (requestNmb + 1);
+        }
+
+        private String preprocessAnswer(String string) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : string.toCharArray()) {
+                if (Character.isDigit(c)) {
+                    sb.append(Character.getNumericValue(c));
+                } else {
+                    sb.append(c);
+                }
+            }
+            return sb.toString();
+        }
+
+        private boolean makeRequest(DatagramSocket socket) {
+            String request = createRequestText();
+            byte[] bytes = request.getBytes();
+            DatagramPacket packet = new DatagramPacket(bytes, bytes.length, serverAddress);
+            System.out.println(request);
+            try {
+                socket.send(packet);
+            } catch (IOException e) {
+                System.err.println("Can't send request to server: " + e.getMessage());
+                return false;
+            }
+            packet.setData(new byte[BUFF_SIZE]);
+            try {
+                socket.receive(packet);
+            } catch (IOException e) {
+                // waiting for answer
+                return false;
+            }
+            String answer = new String(packet.getData(), packet.getOffset(),
+                    packet.getLength(), StandardCharsets.UTF_8);
+            System.out.println(answer);
+            return preprocessAnswer(answer).contains(request);
+        }
+
+        public ClientThread(int threadNmb, int requests, SocketAddress serverAddress, String prefix) {
+            this.threadNmb = threadNmb;
+            this.requests = requests;
+            this.serverAddress = serverAddress;
+            this.prefix = prefix;
+        }
+
+        @Override
+        public void run() {
+            try (DatagramSocket socket = new DatagramSocket()) {
+                BUFF_SIZE = socket.getReceiveBufferSize();
+                socket.setSoTimeout(100);
+                for (requestNmb = 0; requestNmb < requests; requestNmb++) {
+                    while (!makeRequest(socket)) {
+                    }
+                }
+
+            } catch (SocketException e) {
+                System.err.println("Can't connect to server: " + e.getMessage());
+            }
+        }
+    }
+}
---
+++ java-solutions/info/kgeorgiy/ja/kasatov/hello/HelloUDPServer.java
@@ -0,0 +1,83 @@
+package info.kgeorgiy.ja.kasatov.hello;
+
+import info.kgeorgiy.java.advanced.hello.HelloServer;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class HelloUDPServer implements HelloServer {
+    private int port;
+    private int maxThreads;
+    private ExecutorService threadsPool;
+    private DatagramSocket socket;
+    Thread mainThread;
+
+    @Override
+    public void start(int port, int threads) {
+        this.port = port;
+        this.threadsPool = Executors.newFixedThreadPool(threads);
+        mainThread = new Thread(new MainThread(port));
+        mainThread.start();
+    }
+
+    private class MainThread implements Runnable {
+        private static final int BUFF_SIZE = 128;
+        private final int port;
+        public MainThread(int port) {
+            this.port = port;
+        }
+
+        @Override
+        public void run() {
+            try {
+                socket = new DatagramSocket(port);
+                socket.setSoTimeout(100);
+                while (!Thread.interrupted()) {
+                    DatagramPacket packet = new DatagramPacket(new byte[BUFF_SIZE], BUFF_SIZE);
+                    try {
+                        socket.receive(packet);
+                        threadsPool.submit(() -> answer(packet));
+                    } catch (IOException e) {
+                        // Waiting for clients
+                    }
+                }
+                Thread.currentThread().interrupt();
+            } catch (SocketException e) {
+                // TODO
+            }
+        }
+    }
+
+    private void answer(DatagramPacket packet) {
+        try {
+            String request = new String(packet.getData(), 0, packet.getLength());
+            String answer = "Hello, " + request;
+            packet.setData(answer.getBytes());
+            socket.send(packet);
+        } catch (IOException e) {
+            // TODO
+        }
+    }
+
+    @Override
+    public void close() {
+        mainThread.interrupt();
+        threadsPool.shutdownNow();
+        try {
+            if ( !threadsPool.awaitTermination(Long.MAX_VALUE, TimeUnit.DAYS)) {
+                System.err.println("Server subthreads pool wasn't terminated");
+            }
+        } catch (InterruptedException e) {
+            System.err.println("Server subthreads weren't awaited");
+        }
+        try {
+            mainThread.join();
+        } catch (InterruptedException e) {
+            System.err.println("Server main thread wasn't terminated correctly");
+        }
+        socket.close();
+    }
+}
